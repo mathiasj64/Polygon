a8a547e6899fcfab09b81e08b403f998dd17f962
madsr <madsr@10.20.154.27>
2016-03-31 10:54:30 +0200

    Customer objekt lavet færdig, customermapper arbejdet på, burde kunne hente alle customers nu.

--- Polygon/src/java/Mappers/CustomerMapper.java
+++ Polygon/src/java/Mappers/CustomerMapper.java
@@ -5,11 +5,49 @@
  */
 package Mappers;
 
+import Model.Connector;
+import Objects.Customer;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Mathias
  */
 public class CustomerMapper
 {
-  
+
+    public Customer getCustomers()
+    {
+        int customerID;
+        String customerName;
+        String customerEmail;
+        int phoneNum;
+
+        try
+        {
+            Connector c = new Connector();
+
+            String query = "SELECT * FROM customer";
+
+            ResultSet res = c.stmt.executeQuery(query);
+
+            while (res.next())
+            {
+
+                customerID = Integer.parseInt(res.getString(1));
+                customerName = res.getString(2);
+                customerEmail = res.getString(3);
+                phoneNum = Integer.parseInt(res.getString(4));
+
+                return new Customer(customerID, customerName, customerEmail, phoneNum);
+            }
+
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
 }
