ea6b8e980c1018d8ea01f462859d9d186bd9af33
madsr <madsr@10.20.154.27>
2016-03-31 10:30:10 +0200

    Oprettet objects package, tilføjet customer inde i den

---
+++ Polygon/src/java/Objects/Customer.java
@@ -0,0 +1,19 @@
+
+package Objects;
+
+/**
+ *
+ * @author madsr
+ */
+public class Customer
+{
+    private int customerID; 
+    private String customerName;
+    private String customerEmail; 
+    private int phoneNumber; 
+    
+    public Customer()
+    {
+        
+    }
+}
