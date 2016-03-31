4d2c5500c00dfaf4a973bc5cd24d7442d5260e07
madsr <madsr@10.20.154.27>
2016-03-31 10:55:13 +0200

    Customer objekt lavet færdig, customermapper arbejdet på, burde kunne hente alle customers nu.

--- Polygon/src/java/Objects/Customer.java
+++ Polygon/src/java/Objects/Customer.java
@@ -12,8 +12,54 @@ public class Customer
     private String customerEmail; 
     private int phoneNumber; 
     
-    public Customer()
+    public Customer(int cID, String cName, String cEmail, int pNumber)
     {
+        customerID = cID; 
+        customerName = cName; 
+        customerEmail = cEmail; 
+        phoneNumber = pNumber; 
         
     }
+
+    public int getCustomerID()
+    {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID)
+    {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName()
+    {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName)
+    {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail()
+    {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail)
+    {
+        this.customerEmail = customerEmail;
+    }
+
+    public int getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
 }
