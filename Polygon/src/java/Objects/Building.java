151a6a5401b894d8ddc0cf1c3d0b347a5cffca85
Thygesen <Thygesen@10.20.154.81>
2016-03-31 11:17:01 +0200

    Tilføjet building object

--- Polygon/src/java/Objects/Building.java
+++ Polygon/src/java/Objects/Building.java
@@ -11,5 +11,81 @@ package Objects;
  */
 public class Building
 {
-  
+
+    private int BuildingID;
+    private int CustomerID;
+    private String Address;
+    private int ParcelNo;
+
+    public Building(int BuildingID, int CustomerID, String Address, int ParcelNo, int SizeOfBuilding, String AdditionalInformation)
+    {
+        this.BuildingID = BuildingID;
+        this.CustomerID = CustomerID;
+        this.Address = Address;
+        this.ParcelNo = ParcelNo;
+        this.SizeOfBuilding = SizeOfBuilding;
+        this.AdditionalInformation = AdditionalInformation;
+    }
+
+    public int getBuildingID()
+    {
+        return BuildingID;
+    }
+
+    public void setBuildingID(int BuildingID)
+    {
+        this.BuildingID = BuildingID;
+    }
+
+    public int getCustomerID()
+    {
+        return CustomerID;
+    }
+
+    public void setCustomerID(int CustomerID)
+    {
+        this.CustomerID = CustomerID;
+    }
+
+    public String getAddress()
+    {
+        return Address;
+    }
+
+    public void setAddress(String Address)
+    {
+        this.Address = Address;
+    }
+
+    public int getParcelNo()
+    {
+        return ParcelNo;
+    }
+
+    public void setParcelNo(int ParcelNo)
+    {
+        this.ParcelNo = ParcelNo;
+    }
+
+    public int getSizeOfBuilding()
+    {
+        return SizeOfBuilding;
+    }
+
+    public void setSizeOfBuilding(int SizeOfBuilding)
+    {
+        this.SizeOfBuilding = SizeOfBuilding;
+    }
+
+    public String getAdditionalInformation()
+    {
+        return AdditionalInformation;
+    }
+
+    public void setAdditionalInformation(String AdditionalInformation)
+    {
+        this.AdditionalInformation = AdditionalInformation;
+    }
+    private int SizeOfBuilding;
+    private String AdditionalInformation;
 }
