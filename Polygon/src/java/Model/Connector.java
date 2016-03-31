1a2e378ba4481cce18bb14c6276bcc90d221a077
Mathias <Mathias@10.20.145.202>
2016-03-31 10:13:35 +0200

    lavet connector færdig

--- Polygon/src/java/Model/Connector.java
+++ Polygon/src/java/Model/Connector.java
@@ -9,7 +9,35 @@ package Model;
  *
  * @author Mathias
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Mathias
+ */
 public class Connector
 {
+  private final String HOST = "localhost";
+  private int PORT = 3306;
+  private final String DATABASE = "football";
+  private final String USERNAME = "root";
+  private final String PASSWORD = "1234";
+  private final String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
+  public Statement stmt;
   
-}
+  public Connector()
+  {
+    try
+    {
+      Class.forName("com.mysql.jdbc.Driver");
+      Connection con = DriverManager.getConnection(url, USERNAME, PASSWORD);
+      stmt = con.createStatement();
+      System.out.println("Logged in to connector");
+    } catch (SQLException | ClassNotFoundException ex)
+    {
+      System.out.println(ex);
+    }
+  }
