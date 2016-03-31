9b5970d227bf55eb66e3c99d81a1a090e414e7bb
Mathias <Mathias@10.20.145.202>
2016-03-31 11:18:42 +0200

    færdiggjort report objekt

--- Polygon/src/java/Objects/Report.java
+++ Polygon/src/java/Objects/Report.java
@@ -11,5 +11,60 @@ package Objects;
  */
 public class Report
 {
-  //hej
+  
+  int conditionLevel;
+  int buildingID;
+  String descriptionOfBuilding;
+  String functionOfBuilding;
+  
+  public Report(int condition_Level, int building_ID, String description_Of_Building, String function_Of_Building)
+  {
+    conditionLevel = condition_Level;
+    buildingID = building_ID;
+    descriptionOfBuilding = description_Of_Building;
+    functionOfBuilding = function_Of_Building;
+  }
+
+  public void setConditionLevel(int conditionLevel)
+  {
+    this.conditionLevel = conditionLevel;
+  }
+
+  public void setBuildingID(int buildingID)
+  {
+    this.buildingID = buildingID;
+  }
+
+  public void setDescriptionOfBuilding(String descriptionOfBuilding)
+  {
+    this.descriptionOfBuilding = descriptionOfBuilding;
+  }
+
+  public void setFunctionOfBuilding(String functionOfBuilding)
+  {
+    this.functionOfBuilding = functionOfBuilding;
+  }
+  
+  public int getConditionLevel()
+  {
+    return conditionLevel;
+  }
+
+  public int getBuildingID()
+  {
+    return buildingID;
+  }
+
+  public String getDescriptionOfBuilding()
+  {
+    return descriptionOfBuilding;
+  }
+
+  public String getFunctionOfBuilding()
+  {
+    return functionOfBuilding;
+  }
+  
+  
+  
 }
