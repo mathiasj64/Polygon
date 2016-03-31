32a2d39876b9762d4d9565034a4d6a4f06ee419d
Mathias <Mathias@10.20.146.122>
2016-03-31 12:14:41 +0200

    lavet report mapper

--- Polygon/src/java/Mappers/ReportMapper.java
+++ Polygon/src/java/Mappers/ReportMapper.java
@@ -5,6 +5,13 @@
  */
 package Mappers;
 
+import Model.Connector;
+import Objects.Report;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author Mathias
@@ -12,4 +19,39 @@ package Mappers;
 public class ReportMapper
 {
   
+  public ArrayList<Report> reports = new ArrayList<>(); 
+  
+  public ArrayList<Report> getReport()
+  {
+    int buildingID;
+    int conditionLevel;
+    String descriptionOfBuilding;
+    String functionOfBuilding;
+    
+    try
+    {
+      Connector c = new Connector();
+
+      String query = "SELECT * FROM CheckupReport";
+
+      Statement s = c.stmt;
+
+      ResultSet res = s.executeQuery(query);
+
+      while (res.next())
+      {
+        buildingID = Integer.parseInt(res.getString(1));
+        conditionLevel = Integer.parseInt(res.getString(2));
+        descriptionOfBuilding = res.getString(3);
+        functionOfBuilding = res.getString(4);
+        reports.add(new Report(buildingID, conditionLevel, descriptionOfBuilding, functionOfBuilding));
+      }
+                  
+    } catch (SQLException | NullPointerException ex)
+    {
+      ex.printStackTrace();
+      System.out.println(ex);
+    }
+    return reports;
+  }
 }
