dc085cfbcf312f06e0e9b5346c3af5786eb008b9
Mathias <Mathias@DELL-E4310.home>
2016-04-12 13:52:33 +0200

    oprettet completer report servlet

---
+++ Polygon/src/java/controller/CompleteReportServlet.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Mathias
+ */
+public class CompleteReportServlet extends HttpServlet
+{
+
+  /**
+   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+   * methods.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    response.setContentType("text/html;charset=UTF-8");
+    try (PrintWriter out = response.getWriter())
+    {
+      String buildingName = request.getParameter("buildingName");
+      String date = request.getParameter("date");
+      String address = request.getParameter("address");
+      String zipcode = request.getParameter("zipcode");
+      String year = request.getParameter("year");
+      String size = request.getParameter("size");
+      String buildingUse = request.getParameter("buildingUse");
+      String roof = request.getParameter("roof");
+      String outerWalls = request.getParameter("outerWalls");
+      
+      //Facade.getInstance().rm.addReport(blablabla);
+      
+      ServletContext sc = getServletContext();
+      RequestDispatcher rd = sc.getRequestDispatcher("/reportspage.jsp");
+      rd.forward(request, response);
+    }
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /**
+   * Handles the HTTP <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo()
+  {
+    return "Short description";
+  }// </editor-fold>
+
+}
