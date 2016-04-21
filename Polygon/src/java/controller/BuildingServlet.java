ed40b2b8d20589818b4eb3451f8d659a7e258f1f
Mathias <Mathias@10.20.161.231>
2016-04-21 14:59:26 +0200

    tilføjet at man ikke skal skrive sit customer id

--- Polygon/src/java/controller/BuildingServlet.java
+++ Polygon/src/java/controller/BuildingServlet.java
@@ -5,7 +5,6 @@
  */
 package controller;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
@@ -13,6 +12,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -22,74 +22,83 @@ import javax.servlet.http.HttpServletResponse;
 public class BuildingServlet extends HttpServlet
 {
 
-  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException
-  {
-    response.setContentType("text/html;charset=UTF-8");
-    try (PrintWriter out = response.getWriter())
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
     {
-      
-      int CID = Integer.parseInt(request.getParameter("cid")); 
-      int zipcode = Integer.parseInt(request.getParameter("zipcode"));
-      String Address = request.getParameter("Address");
-      int PC = Integer.parseInt(request.getParameter("PC"));
-      int SOB = Integer.parseInt(request.getParameter("SOB"));
-      String AI = request.getParameter("AI");
-      
-      try
-      { 
-      Controller.getInstance().addBuilding(CID, zipcode, Address, PC, SOB, AI);
-      } catch (DataException ex)
-      {
-        out.println("Der opstod en fejl ved oprettelse af bygning. Bygningen er ikke blevet oprettet.");
-      }
-      
-      ServletContext sc = getServletContext();
-      RequestDispatcher rd = sc.getRequestDispatcher("/buildingspage.jsp");
-      rd.include(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter())
+        {
+            int CID;
+            int customerid = (int) request.getSession().getAttribute("customerid");
+            int accessLevel = (int) request.getSession().getAttribute("accessLevel");
+
+            if (accessLevel == 1)
+            {
+                CID = customerid;
+            } else
+            {
+                CID = Integer.parseInt(request.getParameter("cid"));
+            }
+            int zipcode = Integer.parseInt(request.getParameter("zipcode"));
+            String Address = request.getParameter("Address");
+            int PC = Integer.parseInt(request.getParameter("PC"));
+            int SOB = Integer.parseInt(request.getParameter("SOB"));
+            String AI = request.getParameter("AI");
+
+            try
+            {
+                Controller.getInstance().addBuilding(CID, zipcode, Address, PC, SOB, AI);
+            } catch (DataException ex)
+            {
+                out.println("Der opstod en fejl ved oprettelse af bygning. Bygningen er ikke blevet oprettet.");
+            }
+
+            ServletContext sc = getServletContext();
+            RequestDispatcher rd = sc.getRequestDispatcher("/buildingspage.jsp");
+            rd.include(request, response);
+        }
     }
-  }
 
-  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-  /**
-   * Handles the HTTP <code>GET</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException
-  {
-    processRequest(request, response);
-  }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
 
-  /**
-   * Handles the HTTP <code>POST</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException
-  {
-    processRequest(request, response);
-  }
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
 
-  /**
-   * Returns a short description of the servlet.
-   *
-   * @return a String containing servlet description
-   */
-  @Override
-  public String getServletInfo()
-  {
-    return "Short description";
-  }// </editor-fold>
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo()
+    {
+        return "Short description";
+    }// </editor-fold>
 
 }
